61ce6a4ae18abd49aabdb0610c8fba6ec991dba7
John Timms <johngtimms@gmail.com>
2013-11-21 18:29:28 -0800

    Removed RestaurantTimms initialization from Application in prep for merging into master.

--- src/city/Application.java
+++ src/city/Application.java
@@ -69,68 +69,7 @@ public class Application {
 	 * people to create and what roles to create them in.
 	 */
 	private static void parseConfig() {
-		// Set up the staff
-		PersonAgent p1 = new PersonAgent("Cashier 1", date);
-		RestaurantTimmsCashierRole p1r1 = new RestaurantTimmsCashierRole();
-		RestaurantTimmsCashierAnimation p1a1 = new RestaurantTimmsCashierAnimation();
-		p1r1.setAnimation(p1a1);
-		mainFrame.restaurantTimmsPanel.addVisualizationElement(p1a1);
-		p1.setOccupation(p1r1);
-		people.add(p1);
-		p1.startThread();
-		
-		PersonAgent p2 = new PersonAgent("Cook 1", date);
-		RestaurantTimmsCookRole p2r1 = new RestaurantTimmsCookRole(p1r1);
-		RestaurantTimmsCookAnimation p2a1 = new RestaurantTimmsCookAnimation();
-		p2r1.setAnimation(p2a1);
-		mainFrame.restaurantTimmsPanel.addVisualizationElement(p2a1);
-		p2.setOccupation(p2r1);
-		people.add(p2);
-		p2.startThread();
-		
-		PersonAgent p3 = new PersonAgent("Host 1", date);
-		RestaurantTimmsHostRole p3r1 = new RestaurantTimmsHostRole();
-		RestaurantTimmsHostAnimation p3a1 = new RestaurantTimmsHostAnimation();
-		p3r1.setAnimation(p3a1);
-		mainFrame.restaurantTimmsPanel.addVisualizationElement(p3a1);
-		p3.setOccupation(p3r1);
-		people.add(p3);
-		p3.startThread();
-		
-		PersonAgent p4 = new PersonAgent("Waiter 1", date);
-		RestaurantTimmsWaiterRole p4r1 = new RestaurantTimmsWaiterRole(p2r1, p3r1, p1r1, 0);
-		RestaurantTimmsWaiterAnimation p4a1 = new RestaurantTimmsWaiterAnimation(p4r1);
-		p4r1.setAnimation(p4a1);
-		p3r1.addWaiter(p4r1);
-		mainFrame.restaurantTimmsPanel.addVisualizationElement(p4a1);
-		p4.setOccupation(p4r1);
-		people.add(p4);
-		p4.startThread();
-		
-		// Set up the table
-		RestaurantTimmsTableAnimation t1 = new RestaurantTimmsTableAnimation(0);
-		mainFrame.restaurantTimmsPanel.addVisualizationElement(t1);
-		p3r1.addTable(0);
-		
-		// Wait for things to get in position
-		try {
-			Thread.sleep(4000);
-		} catch (InterruptedException e) {}
-		
-		// Send in a customer
-		PersonAgent p5 = new PersonAgent("Customer 1", date);
-		RestaurantTimmsCustomerRole p5r1 = new RestaurantTimmsCustomerRole(p3r1, p1r1);
-		RestaurantTimmsCustomerAnimation p5a1 = new RestaurantTimmsCustomerAnimation(p5r1);
-		p5r1.setAnimation(p5a1);
-		mainFrame.restaurantTimmsPanel.addVisualizationElement(p5a1);
-		p5.addRole(p5r1);
-		people.add(p5);
-		p5.startThread();
-		p5r1.msgGoToRestaurant();
-		
-		// TODO these shouldn't be necessary, figure out why they're needed
-		p5r1.setActive();
-		p5.stateChanged();
+
 	}
 	
 	public static class CityMap {
