d5ff3f87b3bd7b117c7b6eb523f0bc944e275a78
John Timms <johngtimms@gmail.com>
2013-11-21 21:16:25 -0800

    Updated building base class.

--- src/city/Building.java
+++ src/city/Building.java
@@ -6,24 +6,40 @@ package city;
  * Buildings are coded for restaurants, banks, markets, homes, bus stops.
  */
 public abstract class Building {
+	
+	// Data
+	
 	private String name;
-	int cash;
+	private int cash;
+
+	// Constructor
 
 	public Building(String name) {
 		this.name = name;
 	}
-
+    
+    // Messages
+    
+    // Scheduler
+	
+	// Actions
+	
+	// Getters
+	
 	public String getName() {
 		return name;
 	}
 
-	public String toString() {
-		return this.getClass().getName() + ": " + name;
-	}
-	public void setCash(int c){
-		cash = c;
-	}
 	public int getCash(){
 		return cash;
 	}
+	
+	// Setters
+	
+	public void setCash(int c){
+		this.cash = c;
+	}
+	
+	// Utilities 
+
 }
