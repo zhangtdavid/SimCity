d88d4ad68db5b5c01a30acc621883cba5b4cb711
David Zhang <davidtzh@usc.edu>
2013-11-23 13:10:17 -0800

    GUI updated. Doesn't really do anything but show a restaurant.

--- src/city/gui/MainFrame.java
+++ src/city/gui/MainFrame.java
@@ -1,6 +1,10 @@
 package city.gui;
 
 import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.HeadlessException;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
@@ -11,6 +15,12 @@ public class MainFrame extends JFrame {
 	
     private static int WINDOWX = 1100;
     private static int WINDOWY = 700;
+    
+    CityViewPanel cityView;
+	BuildingView buildingView;
+//	TracePanel tracePanel;
+//	CityControlPanel CP;
+	GridBagConstraints c = new GridBagConstraints();
 	
     /**
      * Constructor method
@@ -18,6 +28,44 @@ public class MainFrame extends JFrame {
      * The MainFrame object is the main window of the program GUI.
      */
     public MainFrame() {
+    	// Set up the city control panel
+//    	CP = new CityControlPanel(this);
+		// Set up the trace panel
+//		tracePanel = new TracePanel();
+//		tracePanel.setPreferredSize(new Dimension(CP.getPreferredSize().width, (int)(1.4*CP.getPreferredSize().height)));
+//		tracePanel.showAlertsForAllLevels();
+//		tracePanel.showAlertsForAllTags();
+    	// Set up the city panel
+		cityView = new CityViewPanel(this);
+		// Set up the building panel
+		buildingView = new BuildingView(this);
+		// Set up the info panel
+//		info = new InfoPanel(this);
+		
+		// Layout for gridbaglayout
+		this.setLayout(new GridBagLayout());
+		
+		c.gridx = 0; c.gridy = 0;
+		c.gridwidth = 6; c.gridheight = 6;
+		this.add(cityView, c);
+		
+//		c.gridx = 6; c.gridy = 0;
+//		c.gridwidth = 5; c.gridheight = 1;
+//		this.add(info, c);
+
+		c.gridx = 6; c.gridy = 1;
+		c.gridwidth = 5; c.gridheight = 5;
+		this.add(buildingView, c);
+
+//		c.gridx = 0; c.gridy = 6;
+//		c.gridwidth = 11; c.gridheight = 1;
+//		this.add(CP, c);
+		
+//		c.gridx = 0; c.gridy = 7;
+//		c.gridwidth = 11; c.gridheight = 3;
+//		c.fill = GridBagConstraints.BOTH;
+//		this.add(tracePanel, c);
+		
     	// Set up the window
         this.setTitle("SimCity201");
         this.setVisible(true);
