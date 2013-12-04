5c22ceeb52649481ba0bc14920379edc520bb4a3
John Timms <johngtimms@gmail.com>
2013-12-03 22:21:22 -0800

    Updates to RestaurantTimms tests and mocks.

--- src/city/tests/RestaurantTimmsHostTest.java
+++ src/city/tests/RestaurantTimmsHostTest.java
@@ -7,5 +7,11 @@ public class RestaurantTimmsHostTest extends TestCase {
 	public void setUp() throws Exception {
 		super.setUp();		
 	}
-
+	
+	public void testNormativeScenario() throws InterruptedException {
+		System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
+		
+		assertTrue("", true);
+	}
+	
 }
---
+++ src/city/tests/animations/mock/MockRestaurantTimmsAnimatedCashier.java
@@ -0,0 +1,22 @@
+package city.tests.animations.mock;
+
+import java.awt.Graphics2D;
+
+import city.abstracts.MockAnimation;
+import city.animations.interfaces.RestaurantTimmsAnimatedCashier;
+
+public class MockRestaurantTimmsAnimatedCashier extends MockAnimation implements RestaurantTimmsAnimatedCashier {
+
+	@Override
+	public void updatePosition() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
