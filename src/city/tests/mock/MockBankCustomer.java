cb2a5591624d506a52b97f4842451be2d45908dd
John Timms <johngtimms@gmail.com>
2013-12-04 12:13:19 -0800

    Yeah, I reverted MockBankCustomer.

--- src/city/tests/mock/MockBankCustomer.java
+++ src/city/tests/mock/MockBankCustomer.java
@@ -1,45 +1,18 @@
 package city.tests.mock;
 
 import utilities.LoggedEvent;
-import city.Application;
 import city.Application.BANK_SERVICE;
 import city.Application.TRANSACTION_TYPE;
-import city.BuildingInterface;
 import city.abstracts.MockRole;
-import city.interfaces.Bank;
 import city.interfaces.BankCustomer;
 import city.interfaces.BankTeller;
-import city.roles.BankManagerRole;
 
 public class MockBankCustomer extends MockRole implements BankCustomer {
 	
-	// Data
-	
-	private Bank building;
-	private BuildingInterface business;
-	private Application.TRANSACTION_TYPE depositType;
-	private Application.BANK_SERVICE service;
-
-	private BankManagerRole b;
-	private int netTransaction = 0;
-	private STATE st;
-	private int amount;
-	private BankTeller t;
-	public int acctNum = -1;
-	private int boothNumber;
-
-	// Constructor
-	
-	public MockBankCustomer(BuildingInterface b) {
-		building = null;
-		business = b;
-	}
-	
 	public MockBankCustomer() {
-		building = null;
+		// TODO Auto-generated method stub
+		
 	}
-	
-	// Messages
 
 	@Override
 	public void msgDepositCompleted() {
@@ -80,46 +53,18 @@ public class MockBankCustomer extends MockRole implements BankCustomer {
 		// TODO Auto-generated method stub
 		
 	}
-	
-	// Scheduler
-	
+
 	@Override
-	public boolean runScheduler() {
+	public void setActive(BANK_SERVICE atmdeposit, int i,
+			TRANSACTION_TYPE business) {
+		// TODO Auto-generated method stub
 		
-		if (depositType == TRANSACTION_TYPE.business) {
-			business.setCash(building.getCash() + netTransaction);
-		} else if (depositType == Application.TRANSACTION_TYPE.personal) {
-			this.getPerson().setCash(this.getPerson().getCash() + netTransaction);
-		}
-		netTransaction = 0;
-		super.setInactive();
-		
-		return false;
 	}
-	
-	// Getters
-	
+
 	@Override
 	public BANK_SERVICE getService() {
 		// TODO Auto-generated method stub
 		return null;
-	}
-	
-	// Setters
-
-	@Override
-	public void setActive(BANK_SERVICE s, int money, TRANSACTION_TYPE t) {
-		log.add(new LoggedEvent("Received setActive"));
-		this.service = s;
-		this.depositType = t;
-		this.amount = money;
-		this.st = STATE.entering;
-		if (s == BANK_SERVICE.atmDeposit || s == BANK_SERVICE.deposit) {
-			this.netTransaction = (-money);
-		} else if (s == BANK_SERVICE.moneyWithdraw) {
-			this.netTransaction = money;
-		}
-		super.setActive();
 	}
 
 }
